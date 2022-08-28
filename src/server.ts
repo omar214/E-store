7fa72306ccf00ae903cd50f68cae4e376882b19c
omar214 <omarmohamed2322000@gmail.com>
2022-08-28 18:29:52 +0200

    add server

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response, NextFunction } from 'express';
+import morgan from 'morgan';
+import { handleNotFound, handleRouteError } from './middlewares/errorHandler';
+import allRoutes from './api';
+import config from './config';
+import db from './database';
+
+const app = express();
+
+if (config.NODE_ENV === 'dev') {
+	app.use(morgan('dev'));
+}
+app.use(express.urlencoded({ extended: true })); // send nested objects
+app.use(express.static('public')); // serve static files
+
+app.listen(config.PORT, () => {
+	console.log('Server listening on Port', config.PORT);
+});
+
+// Routes which should handle requests
+app.use('/api', allRoutes);
+app.use(handleRouteError); // handle errors
+app.use('*', handleNotFound); // handle not found routes
+
+export default app;
