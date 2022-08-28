9d8567590bad107dfd23d33a41d406f658a29f39
omar214 <omarmohamed2322000@gmail.com>
2022-08-28 20:43:58 +0200

    add default constraint

--- migrations/sqls/20220828162403-orders-table-up.sql
+++ migrations/sqls/20220828162403-orders-table-up.sql
@@ -2,7 +2,7 @@
 CREATE TABLE orders(
   id SERIAL PRIMARY KEY , 
   userId INTEGER NOT NULL, 
-  status VARCHAR(20) , 
+  status VARCHAR(20) DEFAULT 'Pending' NOT NULL, 
 
   FOREIGN KEY(userId) REFERENCES users(id)
 );
