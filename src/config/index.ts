5fcf457ed4db0602c360465c160add335fee8705
omar214 <omarmohamed2322000@gmail.com>
2022-08-28 18:27:48 +0200

    add config

---
+++ src/config/index.ts
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const {
+	PORT,
+	NODE_ENV,
+	PG_HOST,
+	PG_PORT,
+	PG_DATABASE_DEV,
+	PG_DATABASE_TEST,
+	PG_PASSWORD,
+	PG_USER,
+} = process.env;
+
+const DATABASE =
+	NODE_ENV === 'dev' ? PG_DATABASE_DEV : PG_DATABASE_TEST;
+
+const config = {
+	PORT: PORT || 5000,
+	NODE_ENV,
+	PG_HOST,
+	PG_PORT,
+	PG_DATABASE: DATABASE,
+	PG_PASSWORD,
+	PG_USER,
+};
+export default config;
