7154a3a1d648f4e03c2aa1a5ba7474ec19b6dd9a
omar214 <omarmohamed2322000@gmail.com>
2022-08-28 21:37:32 +0200

    edit user-table

--- migrations/sqls/20220828161537-users-table-up.sql
+++ migrations/sqls/20220828161537-users-table-up.sql
@@ -5,5 +5,5 @@ CREATE TABLE users (
   email VARCHAR(50) NOT NULL, 
   first_name VARCHAR(50) NOT NULL, 
   last_name VARCHAR(50) NOT NULL, 
-  password VARCHAR(50) NOT NULL
+  password VARCHAR(500) NOT NULL
 );
