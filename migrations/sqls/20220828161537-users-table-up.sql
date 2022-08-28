b24715c08c96ff98fefc0a1ff7fcd335d1852adb
omar214 <omarmohamed2322000@gmail.com>
2022-08-28 19:17:04 +0200

    edit migrate

--- migrations/sqls/20220828161537-users-table-up.sql
+++ migrations/sqls/20220828161537-users-table-up.sql
@@ -2,6 +2,7 @@
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY , 
+  email VARCHAR(50) NOT NULL, 
   first_name VARCHAR(50) NOT NULL, 
   last_name VARCHAR(50) NOT NULL, 
   password VARCHAR(50) NOT NULL
