af4124d317ded550b5f15625ce3fa54cc94f38d6
omar214 <omarmohamed2322000@gmail.com>
2022-09-01 03:47:24 +0200

    update migrations

--- migrations/sqls/20220828162403-orders-table-up.sql
+++ migrations/sqls/20220828162403-orders-table-up.sql
@@ -1,8 +1,9 @@
 /* Replace with your SQL commands */
+CREATE TYPE STATUS ENUM 
 CREATE TABLE orders(
   id SERIAL PRIMARY KEY , 
   userId INTEGER NOT NULL, 
-  status VARCHAR(20) DEFAULT 'Pending' NOT NULL, 
+  status VARCHAR(20) DEFAULT 'open' NOT NULL CHECK (status IN ('open', 'complete')), 
 
   FOREIGN KEY(userId) REFERENCES users(id)
 );
